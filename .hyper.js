77f71a06dd98616c15d32a9eaea4b37d5392b598
Bart Veneman <bartveneman@users.noreply.github.com>
2018-08-15 22:33:16 +0200

    Adds simple Project Wallace color scheme

--- .hyper.js
+++ .hyper.js
@@ -9,7 +9,7 @@ module.exports = {
     updateChannel: 'stable',
 
     // default font size in pixels for all tabs
-    fontSize: 13.5,
+    fontSize: 14,
 
     // font family with optional fallbacks
     fontFamily: 'Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
@@ -23,12 +23,28 @@ module.exports = {
     shellArgs: ['--login'],
 
     // for environment variables
-    env: {},
+    env: {
+      PURE_PROMPT_SYMBOL: '⚡︎'
+    },
 
     // set to false for no bell
     bell: false,
 
+    // The number of rows to be persisted in terminal buffer for scrolling
+    scrollback: 10000,
+
     // for advanced config flags please refer to https://hyper.is/#cfg
+
+    // WALLACE THEME
+    backgroundColor: 'hsl(203.3, 27.3%, 12.9%)',
+    foregroundColor: 'hsl(198, 5%, 97.3%)',
+    cursorColor: '#24ad6d',
+    selectionColor: 'hsla(198.6, 19.6%, 20.8%, .3)',
+    colors: {
+      blue: '#24ad6d',
+      red: 'hsl(0, 70%, 51%)',
+      magenta: 'hsl(42, 100%, 55%)',
+    }
   },
 
   // a list of plugins to fetch and install from npm
@@ -39,7 +55,7 @@ module.exports = {
   //   `project#1.0.1`
   plugins: [
     'hypercwd', // Open a new tab in the same dir as the current one
-    'hyper-snazzy', // Theme
+    // 'hyper-snazzy', // Theme
     'hyper-tab-icons', // Tab icons
     'hyperterm-cursor', // Make text underneath cursor visible
   ],
