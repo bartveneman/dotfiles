ae78150e0ad1a77d2a31f7b3a6e1ccd776bc99d1
Bart Veneman <1536852+bartveneman@users.noreply.github.com>
2018-11-30 15:13:52 +0100

    Adds aliases for bin/magento and bin/console

--- env.sh
+++ env.sh
@@ -4,8 +4,12 @@ export GPG_TTY=$(tty)
 
 # Aliases
 alias rm=trash
-alias cat=ccat
+alias cat=bat
 alias hr="hr '-'"
+alias mag="bin/magento"
+alias magento="bin/magento"
+alias console="bin/console"
+alias cons="bin/console"
 
 # Functions
 function mkcd() { mkdir -p "$@" && cd "$_"; }
