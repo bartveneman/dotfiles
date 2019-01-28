78638904b4d24deae92c6aed5415f33b500977fa
Bart Veneman <1536852+bartveneman@users.noreply.github.com>
2019-01-28 14:01:07 +0100

    rm magento/composer aliases

--- env.sh
+++ env.sh
@@ -7,10 +7,6 @@ export LC_ALL=en_US.UTF-8
 # Aliases
 alias rm=trash
 alias cat=bat
-alias mag="bin/magento"
-alias magento="bin/magento"
-alias console="bin/console"
-alias cons="bin/console"
 
 # Functions
 function mkcd() { mkdir -p "$@" && cd "$_"; }
